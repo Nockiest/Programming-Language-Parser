cf8b82d31580eff36f6f8a1e9698dfab9a4db552
Andrew <ondralukes06@seznam.cz>
2025-04-08 16:15:39 +0200

    1+1 works!!

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-code = "1+1"
-code2 = "1+1*2"
-code3 = "1+1*2+3"
-code4 = "1+1*2+3*4"
-
-gramatika = {
-    "S": ['A', 'S.'],
-    "S.": ['+', 'A', 'S.'],
-    'S.': [None],
-    "A": ['B', 'A.'],
-    "A.": ['*', 'B', 'A.'],
-    'A.': [None],
-    "B": ['(', 'S', ')'],
-    'B': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-}
-
+code:str = "1 + 1"
+code2 = "1 + 1 * 2"
+ 
 #tokens are added in the compile function
-tokens = []
+tokens = code2.split()
+parseProgress:int = 0
 
+def parseTokens(number:int=1):
+    global tokens
+    global parseProgress
+    takenOut:str
+    if parseProgress >= len(tokens):
+        Exception("Invalid input in parseTokens function")
+    if number ==1:
+        takenOut = tokens[parseProgress]
+    else:
+      takenOut = tokens[parseProgress:(parseProgress +number)]
+    parseProgress += number
+    return takenOut
 
 class Node:
     def __init__(self, value, left, right):
@@ -45,67 +45,94 @@ class NodeTimes(Node):
         return self.left.get_value() * self.right.get_value()
 
 
-def A(lfs: str):
-    if lfs[0] == '': # this shouldnt throw an error
+def A():
+    global tokens
+    global parseProgress
+    if tokens[parseProgress] == '' or  parseProgress > len(tokens): # this shouldnt throw an error
         Exception("Invalid input in A function")
-    if lfs[0] != '':
-        resB = B(lfs)
-        resA = A_(lfs, resB)
+    if tokens[parseProgress] != '':
+        resB = B( )
+        resA = A_(  resB)
         return resA
     else:
         Exception("Invalid input in A function")
 
-def A_(lfs: str, resB):
-    match lfs[0]:
+def A_(  resB):
+    global tokens
+    global parseProgress
+    if parseProgress >= len(tokens):
+        return resB
+    match tokens[parseProgress]:
         case '*':
-            resB = B(lfs[1:])
-            resA_ = A_(lfs[1:], resB)
-            return NodeTimes(lfs , resA_)
+            parseProgress += 1
+            resB = B(  )
+            resA_ = A_(  resB)
+            return NodeTimes(resB, resA_)
         case None:
             Exception("Invalid input in A_ function")
         case _:
             return resB
 
 def S( ):
-    resA = A(lfs)
-    resS = S_(lfs[:len(resA.value)], resA)
+    global tokens
+    global parseProgress
+    resA = A( )
+    resS = S_(resA)
     return resS
  
-def S_(lfs: str, resA):
-    match lfs[0]:
-        case '+':
-            resA = A(lfs[1:])
-            resS = S_(lfs[1:], resA)
-            return resS
-        case '':
-            return resA
-        case _:
-            Exception("Invalid input in S function")
-    
+def S_( resA):
+    global tokens
+    global parseProgress
+    if parseProgress >= len(tokens):
+        return resA
+    elif tokens[parseProgress] == '+':
+        parseTokens()
+        resA = A()
+        resS = S_(resA)
+        return NodeAdd(resS, resA)
+    # elif tokens[parseProgress] == '':
+    #     return resA
+    else:
+        Exception("Invalid input in S function")
     return
     
 
 
-def B(lfs: str):
-    match lfs[0]:
+def B( ):
+    global tokens
+    global parseProgress
+    match   tokens[parseProgress]:
         # case '(':
-        #     resS = S(lfs[1:])
+        #     resS = S()
         #     if lfs[0] != ')':
         #         Exception("Invalid input in B function")
         #     return resS
         case '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9':
-            return Node(int(lfs[0]), None, None)
+            number = parseTokens(1)
+            return Node(number, None, None)
         case _:
             Exception("Invalid input in B function")
-    return Node(lfs[0], None, None)
+    # return Node(lfs[0], None, None)
 
 
 def compile(code: str):
     print(code)
-    tokens.append(code.split()) # wont work for n>1 digit numbers
     res = S()
     print(res)
     return
 
 
 compile(code)
+
+
+
+# gramatika = {
+#     "S": ['A', 'S.'],
+#     "S.": ['+', 'A', 'S.'],
+#     'S.': [None],
+#     "A": ['B', 'A.'],
+#     "A.": ['*', 'B', 'A.'],
+#     'A.': [None],
+#     "B": ['(', 'S', ')'],
+#     'B': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+# }
